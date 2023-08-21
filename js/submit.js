313b6a8eccc836afc858ecb0242925ecf3160538
The3Null4Player613310 <the.3null.4player.6.13.3.10.@gmail.com>
2023-08-21 03:04:00 +0000

    Debug Code

--- js/submit.js
+++ js/submit.js
@@ -1,6 +1,31 @@
+const domain="";
+const token="";
+const id="";
+
 function submit()
 {
-  alert(document.cookie);
+  var data = {};
+  var c = document.cookie.split("; ");
+  
+  for(var i=0; i<c.length; i++)
+  {
+    kv = c[i].split("=");
+    data[kv[0]] = kv[1];  
+  }
+
+  alert(JSON.stringify(data));
+
+  function s(){};
+
+  //$.ajax({
+  //  type: "PATCH",
+  //  datatype: "json",
+  //  url: atob(domain) + "database/rows/table" + id + "/" + sid + "/?user_field_names=true",
+  //  headers: { "authorization":"Token " + atob(token),
+  //             "content-type":"application/json" },
+  //  data JSON.stringify(data),
+  //  success: s
+  //});
 }
 
 submit();
